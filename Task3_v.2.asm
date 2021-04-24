d965e8afe81008ba09b58b8b7d08dbc4d2742d02
Stanislav Kurnevich <70342945+Stan1slavssKy@users.noreply.github.com>
2021-04-24 15:14:45 +0300

    Update Task3_v.2.asm

--- Task3_v.2.asm
+++ Task3_v.2.asm
@@ -1,82 +1,94 @@
+;==========================================================================
+; 
+; This program changes the case of the letters to the opposite and does not affect any other characters.
+; Input parameters: 
+;     There is no any parameters
+; Output parameters:
+;     There is no any parameters
+; Destroyed registers: 
+;     rax, rsi, rcx    	 
+;
+;==========================================================================
+
 section .text
 global _start
 
 ;==========================================================================
+_start:                
+		call read                
 
-_start:
-    mov dl, 'a'
-    sub dl, 'A'                  
-   
-    call read                    
-    call reg_xor                 
-    call replace                  
-    call write                   
-    call end                     
+		mov dl, 'a'
+		sub dl, 'A'					; Found the difference between upper and lower case
+
+		xor rcx, rcx				; Reset registrations to zero
+		call replace
+
+		call write                   
+		call end                     
 
 ;==========================================================================
-
-; reads from the command line to the buffer
+;Reading the buffer to the command line
 read:
-    mov rax, 0                 
-    mov rdi, 0                 
-    mov rsi, buff              
-    mov rdx, num_of_buf        
-    syscall                    
-    ret                        
+		mov rax, 0           		      
+		mov rdi, 0                 	
+		mov rsi, buff              	
+		mov rdx, num_of_buf        	
+		syscall  
+
+		ret                        
 
 ;==========================================================================
-
-; resets registers for future use
-reg_xor:
-    xor rcx, rcx               ; reset the counter
-    xor rax, rax 
-    ret
-
-;==========================================================================
-
+;Changes the case of letters in the buffer
 replace:
-    mov al, byte [buff + rcx]  ; положили i ый элемет буфера
+		mov al, byte [buff + rcx]  
+	
+		cmp al, 'a'
+		jae toupper
 
-    cmp al, 141
-    je  tolower
-    jge toupper 
+		cmp al, 'A'
+		jae tolower
 
-    next:
+		jmp next
+
+tolower:
+		cmp al, 'Z'
+		ja next
+		add al, dl 
+		jmp next
+
+toupper:
+		cmp al, 'z'
+		ja next
+   		sub al, dl	
+		jmp next
+
+next:
         mov byte [buff + rcx], al  
-        inc rcx                    ; rcx++
+        inc rcx                    
 
-        cmp rcx, num_of_buf        ; сравниваем rcx с 100
-        jne replace                ; если не равно 100 то идем на метку Replace если равно то идем дальше по коду
+		cmp byte [buff + rcx + 1], 0
+        jne replace                
+
         ret
 
 ;==========================================================================
-
-;
-tolower:
-    add al, dl 
-    jmp next
-
-toupper:
-    sub al, dl
-    jmp next
-
-;==========================================================================
-
+;Printing the buffer to the command line
 write:
-    mov rax, 1                 ; вывод
-    mov rdi, 1                 ; stdout
-    mov rsi, buff              ; указатель на буффер
-    mov rdx, num_of_buf        ; количество символов
-    syscall                    ; обращение к ядру
-    ret
+		mov rax, 1                 
+		mov rdi, 1                 
+		mov rsi, buff              
+		mov rdx, num_of_buf        
+		syscall                    
+		
+		ret
 
 ;==========================================================================
-
-end:                           ; The end of the program
-    mov rax, 60                ; выход           
-    xor rdi, rdi           
-    syscall                    ; обращение к ядру
-    ret
+; The end of the program
+end:                           
+		mov rax, 60                          
+		xor rdi, rdi           
+		syscall                    
+		ret
 
 ;==========================================================================
 
@@ -84,6 +96,6 @@ section .data
 
 num_of_buf equ 100
 
-buff:  times num_of_buf db 0
+buff:  times num_of_buf db 0x0
 
 ;==========================================================================
